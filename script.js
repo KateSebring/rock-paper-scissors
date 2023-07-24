416ed7fa20f6a8ab2e3d43136bd8d2a9b7de2bb4
KateSebring <ihatesuperman99@gmail.com>
2023-07-24 17:19:36 -0400

    Create function to prompt player for their choice and return it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let computerChoice;
+let playerChoice;
 
 function getComputerChoice() {
     let getNum = Math.floor(Math.random() * 3);
@@ -12,6 +13,17 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getPlayerChoice() {
+    playerChoice = prompt("Choose rock, paper, or scissors!").toLowerCase();
+    return playerChoice;
+}
+
+function playRound(computerChoice, playerChoice) {
+
+}
+
+getPlayerChoice()
 getComputerChoice();
 
 console.log(computerChoice);
+console.log(playerChoice);
