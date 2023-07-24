96865dfa339a497553203bed7783bf7576838ca1
KateSebring <ihatesuperman99@gmail.com>
2023-07-24 17:23:35 -0400

    Create function for playing one round and added tie dialogue - also move variables into functions

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-let computerChoice;
-let playerChoice;
-
 function getComputerChoice() {
+    let computerChoice;
+
     let getNum = Math.floor(Math.random() * 3);
     if (getNum === 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (getNum === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     return computerChoice;
 }
 
 function getPlayerChoice() {
+    let playerChoice;
     playerChoice = prompt("Choose rock, paper, or scissors!").toLowerCase();
     return playerChoice;
 }
 
 function playRound(computerChoice, playerChoice) {
-
+    let winnerOfRound;
+    if ((computerChoice === "rock" && playerChoice === "rock") 
+    || (computerChoice === "paper" && playerChoice === "paper") 
+    || (computerChoice === "scissors" && playerChoice === "scissors")) {
+        winnerOfRound = "It's a tie! No winner.";
+    }
 }
 
-getPlayerChoice()
-getComputerChoice();
 
-console.log(computerChoice);
-console.log(playerChoice);
+console.log(getComputerChoice());
+console.log(getPlayerChoice());
