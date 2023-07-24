e3e7f96aa294f5d0efe3dde9aa0f004182ee57af
KateSebring <ihatesuperman99@gmail.com>
2023-07-24 17:54:01 -0400

    Add function to capitalize winning choice

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function generateNum() {
     return getNum = Math.floor(Math.random() * 3);
 }
 
+function capitalizeChoice(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 function getComputerChoice() {
     let computerChoice = generateNum();
+
     if (computerChoice === 0) {
         computerChoice = "rock";
     } else if (computerChoice === 1) {
@@ -15,9 +20,7 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let playerChoice;
-    playerChoice = prompt("Choose rock, paper, or scissors!").toLowerCase();
-    return playerChoice;
+    return playerChoice = prompt("Choose rock, paper, or scissors!").toLowerCase();
 }
 
 function playRound(computerChoice, playerChoice) {
@@ -25,11 +28,11 @@ function playRound(computerChoice, playerChoice) {
     if ((computerChoice === "scissors" && playerChoice === "rock")
     || (computerChoice === "paper" && playerChoice === "scissors")
     || (computerChoice === "rock" && playerChoice === "paper")) {
-        winnerOfRound = `You win! ${playerChoice} beats ${computerChoice}.`;
+        winnerOfRound = `You win! ${capitalizeChoice(playerChoice)} beats ${computerChoice}.`;
     } else if ((computerChoice === "rock" && playerChoice === "scissors")
     || (computerChoice === "scissors" && playerChoice === "paper")
     || (computerChoice === "paper" && playerChoice === "rock")) {
-        winnerOfRound = `You lose! ${computerChoice} beats ${playerChoice}.`;
+        winnerOfRound = `You lose! ${capitalizeChoice(computerChoice)} beats ${playerChoice}.`;
     } else {
         winnerOfRound = "It's a tie! No winner.";
     }
@@ -39,7 +42,7 @@ function playRound(computerChoice, playerChoice) {
 function game() {
     let computerChoice;
     let playerChoice;
-    
+
     for (i = 0; i < 5; i++) {
         computerChoice = getComputerChoice();
         playerChoice = getPlayerChoice();
